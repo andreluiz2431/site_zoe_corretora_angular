c795ef89eb70c80579dddcdb8fce0c9a3ad895ea
André Luiz <42250150+andreluiz2431@users.noreply.github.com>
2025-05-24 16:53:07 -0300

    adicionando server para railway

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+const DIST_DIR = path.join(__dirname, 'dist/site-zoe-corretora-angular');
+
+app.use(express.static(DIST_DIR));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(DIST_DIR, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
